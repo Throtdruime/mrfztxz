b708f0ddd366ed85a0d3d982451fef4ea2082156
epyuri <14885236+epyuri@user.noreply.gitee.com>
2025-12-30 17:20:22 +0800

    v 1.0

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ import "./js/console/main.js";
 document.getElementById("lianXi").onclick = () => {
   alert(
       "- 作者 QQ 3412725994  -\n" +
+      "- GitHub https://github.com/epYuriX/mrfztxz - \n" +
       "- 交流群 懒得建群  -\n"
   );
 }
